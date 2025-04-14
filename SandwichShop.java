20115c91afe05676763265a2b6f0aeb2f22563ee
Jakariw <jakjwilson01@gmail.com>
2025-04-14 15:28:40 -0700

    SandwichShop.java

--- SandwichShop.java
+++ SandwichShop.java
@@ -11,16 +11,34 @@ public class SandwichShop {
         System.out.println("Which sandwich do you prefer 1 or 2: ");
         int sandwichChoice = scanner.nextInt();
 
+
+        double loadedExtra;
         double basePrice;
+        
         if (sandwichChoice == 1) {
             basePrice = 14.50;
+            loadedExtra = 2.50;
         } else if (sandwichChoice == 2) {
             basePrice = 23.00;
+            loadedExtra = 4.00;
         } else{
             System.out.println("Invalid option, Select 1 or 2.");
             scanner.close();
             return;
         }
+
+        scanner.nextLine();
+        System.out.println("Would you like to upgrade your sandwich to loaded? (Yes/No): ");
+        String loadedSandwich = scanner.nextLine().toLowerCase();
+
+        if (loadedSandwich.equals("yes")) {
+            basePrice += loadedExtra;
+        } else if (!loadedSandwich.equals("No")) {
+            System.out.println("Invalid option for loaded sandwich");
+            scanner.close();
+            return;
+        }
+        
         System.out.println("How old are you: ");
         int age = scanner.nextInt();
         double discount = 0.0;
